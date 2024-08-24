782f069bc58cd5001e3ce5ef34c4af382f5a0a84
canbe <canbe@CANBERK>
2024-08-25 00:58:26 +0300

    Add project files.

---
+++ PracticeList/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<string> visitors = new List<string>();
+visitors.Add("Bülent Ersoy");
+visitors.Add("Ajda Pekkan");
+visitors.Add("Hadise");
+visitors.Add("Hande Yener");
+visitors.Add("Tarkan");
+visitors.Add("Funda Arar");
+visitors.Add("Demet Akalın");
+
+Console.WriteLine("------------------VISITORS-----------------");
+
+for  (int i = 0; i < visitors.Count; i++)
+{
+    Console.WriteLine(visitors[i]);
+}
